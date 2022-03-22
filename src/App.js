db581828cd2590d4e467ecf68ffb69fe13a26c70
Adrian M. Popan <adrian.popan@gmail.com>
2022-03-21 20:00:17 -0500

    add uuid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { Card, Navbar } from './components'
+import {v4 as uuidv4} from 'uuid'
 import data from './data'
 
 function App() {
 
-  const cards = data.map(x => <Card key={data.id} {...x} />)
+  const cards = data.map(x => <Card key={uuidv4()} {...x} />)
   return (
     <div className="App">
       <Navbar />
